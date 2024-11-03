d238472f6b8d4857e63901dbce33827c57751f22
Fass2017SSK! <severine.kombo@epitech.eu>
2024-11-03 18:09:36 +0100

    corrections

--- src/AdminLogin.js
+++ src/AdminLogin.js
@@ -7,7 +7,7 @@ const AdminLogin = ({ onLogin }) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('https://severinekombo.onrender.com/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -33,7 +33,7 @@ const AdminLogin = ({ onLogin }) => {
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Connexion Admin 
+                        Connexion Admin
                     </h2>
                 </div>
                 <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
